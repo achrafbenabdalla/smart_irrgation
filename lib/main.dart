b374978f37da65d37f2df8d167d6ea1edf961b62
soulaima23 <kottisoulayma@gmail.com>
2023-08-28 22:35:43 +0100

    merge with master

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
             .apply(bodyColor: Colors.white),
         canvasColor: secondaryColor,
       ),
-      home: mainpage(),
+      home: authAdmin(),
     );
   }
 }
